68045740a537492ef3ef80f73695db59b05c9162
MOHAN KUMAR R <mohankumar.it.2001@gmail.com>
2025-01-06 21:10:25 +0530

    removed certs

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"crypto/tls"
 	_ "embed"
 	"github.com/MOHANKUMAR-IT/Whisper/internal/signaling"
 	"github.com/MOHANKUMAR-IT/Whisper/internal/web"
@@ -10,25 +9,25 @@ import (
 	"net/http"
 )
 
-// Embed the certificate files
+//// Embed the certificate files
+////
+////go:embed certs/server.crt
+//var serverCert []byte
 //
-//go:embed certs/server.crt
-var serverCert []byte
-
-//go:embed certs/server.key
-var serverKey []byte
+////go:embed certs/server.key
+//var serverKey []byte
 
 func main() {
 
 	// Load the embedded certificate and key
-	cert, err := tls.X509KeyPair(serverCert, serverKey)
-	if err != nil {
-		log.Fatalf("failed to load TLS certificates: %v", err)
-	}
+	//cert, err := tls.X509KeyPair(serverCert, serverKey)
+	//if err != nil {
+	//	log.Fatalf("failed to load TLS certificates: %v", err)
+	//}
 	// Setup the TLS configuration
-	tlsConfig := &tls.Config{
-		Certificates: []tls.Certificate{cert},
-	}
+	//tlsConfig := &tls.Config{
+	//	Certificates: []tls.Certificate{cert},
+	//}
 	router := gin.Default()
 
 	signaling.Register(router)
@@ -37,14 +36,14 @@ func main() {
 
 	// Create the HTTPS server with Gin
 	server := &http.Server{
-		Addr:      ":8080",
-		Handler:   router,
-		TLSConfig: tlsConfig,
+		Addr:    ":8080",
+		Handler: router,
+		//TLSConfig: tlsConfig,
 	}
 
 	// Start the server with HTTPS
 	log.Println("Starting Gin server on https://localhost:8080")
-	if err := server.ListenAndServeTLS("", ""); err != nil {
+	if err := server.ListenAndServe(); err != nil {
 		log.Fatalf("Server failed: %v", err)
 	}
 }
